c7914713a05b6398fd37c189b394e5cf69d49666
Yazichnik86 <a_s_gorbunov@list.ru>
2025-01-28 23:30:34 +0300

    Proj3_0.0.4+new ветка+новая строка

--- src/main/java/com/stepup/NJC1.java
+++ src/main/java/com/stepup/NJC1.java
@@ -5,5 +5,7 @@ public class NJC1 {
         System.out.println("11222");
         System.out.println("1112221");
         System.out.println("333222");
+        System.out.println("33322200000");
+        System.out.println("33322200000");
     }
 }
