4bdf7db19c6e74375a845b0807870a60a421c9a3
Yazichnik86 <a_s_gorbunov@list.ru>
2025-01-28 23:16:45 +0300

    Proj3_0.0.3+логика

--- src/main/java/com/stepup/NJC1.java
+++ src/main/java/com/stepup/NJC1.java
@@ -2,7 +2,8 @@ package com.stepup;
 
 public class NJC1 {
     public static void main(String[] args) {
-        System.out.println("111222");
-        System.out.println("111222");
+        System.out.println("11222");
+        System.out.println("1112221");
+        System.out.println("333222");
     }
 }
